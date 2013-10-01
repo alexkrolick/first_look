0238fe74fc2e2980df774f946469ffa700b6cffa
Alex Krolick <amk283@cornell.edu>
2013-10-01 19:36:29 -0400

    fixed duplicate file contents

--- first_look.py
+++ first_look.py
@@ -1,14 +1,32 @@
-# usage (type in terminal):
-# python first_look_previewer.py file_to_preview
-# expected to be in same directory as first_look.py
+'''Simple data previewer.'''
 
-# alternately, just open python and import the first_look
-# module (this is better and has more options)
+import matplotlib.pyplot as plt
+import pandas
 
-# run help(first_look) for further instructions
+def look(datafile,x_col=0,plot_type="scatter",delimiter="\t"):
+	'''A simple graphical data previewer that plots columns against
+	   each other. Expects data to be stored in columns with one row
+	   of headers inside a delimited text file, i.e., .tsv or .csv.
+	   You need pandas and matplotlib installed to use this module.
 
-import sys
-import first_look as fl
-datafile = sys.argv[1]
+	   Required inputs: datafile - path to file to preview (string)
 
-fl.look(datafile)
+	   Optional inputs: x_col - which column to use for x-axis
+								(int, index starts at 0)
+						plot_type - "scatter" or "line" plot (string)
+						delimiter - data file delimiter (string),
+									usually tab ("\t") or comma (",")
+
+		Recommended usage:
+			import first_look as fl
+			fl.look("your_data_file")
+	'''
+	if plot_type == "scatter":
+		sty = "o"
+	else:
+		sty = "-"
+
+	df = pandas.read_csv(datafile,sep=delimiter) # create a dataframe
+	df.plot(x=x_col,title=datafile,style=sty) # create a matplotlib object
+	plt.show() # show the plot
+
